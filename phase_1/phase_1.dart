6a3fb90a8580007645cc4ea6d38453964742aeb2
tajulkhan <tajulbasheer@gmail.com>
2025-03-02 18:22:22 +0530

    operators

--- phase_1/phase_1.dart
+++ phase_1/phase_1.dart
@@ -1,4 +1,4 @@
-void main() {
+void datatypes() {
   String name = 'Taj'; // Explicitly typed
   var age = 25; // Dart infers the type (int)
   dynamic anything = "Hello"; // Can hold any type of value
@@ -6,4 +6,19 @@ void main() {
   print(name);
   print(age);
   print(anything);
+}
+
+void operators() {
+  int a = 10, b = 3;
+  print(a + b); // Addition
+  print(a - b); // Subtraction
+  print(a * b); // Multiplication
+  print(a / b); // Division (double result)
+  print(a ~/ b); // Integer division
+  print(a % b); // Modulus (remainder)
+}
+
+void main() {
+  datatypes();
+  operators();
 }
