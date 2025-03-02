828cbdb381f29b0662396559daee8f0a1d3fff32
tajulkhan <tajulbasheer@gmail.com>
2025-03-02 18:17:58 +0530

    Variables & Data Types

---
+++ phase_1/phase_1.dart
@@ -0,0 +1,9 @@
+void main() {
+  String name = 'Taj'; // Explicitly typed
+  var age = 25; // Dart infers the type (int)
+  dynamic anything = "Hello"; // Can hold any type of value
+
+  print(name);
+  print(age);
+  print(anything);
+}
