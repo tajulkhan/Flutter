667cd6992712220fb2d00502ebf2ac18d5e0d060
Tajul Khan BS <78636360+tajulkhan@users.noreply.github.com>
2025-03-06 20:02:15 +0530

    Update basic.dart

--- basic.dart
+++ basic.dart
@@ -29,3 +29,21 @@ void main() {
   Person p = Person();
   p.greet();
 }
+
+//Parameterized Constructor
+class Person {
+  String name;
+
+  // Parameterized Constructor
+  Person(this.name);
+
+  void greet() {
+    print('Hello, my name is $name');
+  }
+}
+
+void main() {
+  Person p = Person('Ali');
+  p.greet(); // Output: Hello, my name is Ali
+}
+
