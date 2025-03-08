086b72bf540ecd477bb0a7808e082921799878ff
tajulkhan <tajulbasheer@gmail.com>
2025-03-09 04:49:33 +0530

    Finally Block (Always Executes)

--- oop.dart
+++ oop.dart
@@ -20,3 +20,15 @@ void main() {
     print('Invalid number format!');
   }
 }
+
+// Finally Block (Always Executes)
+void main() {
+  try {
+    int result = 10 ~/ 2;
+    print(result);
+  } catch (e) {
+    print('Error: $e');
+  } finally {
+    print('Execution complete.');
+  }
+}
