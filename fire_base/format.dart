af9516dbebf660923034b1adcb1c2fb0262f368e
Tajul Khan BS <78636360+tajulkhan@users.noreply.github.com>
2025-03-13 23:50:48 +0530

    Update format.dart

--- fire_base/format.dart
+++ fire_base/format.dart
@@ -2,7 +2,6 @@ String formatCategoryName(String category) {
   List<String> words = category.split('_'); // Split by underscore
   return words.map((word) => '${word[0].toUpperCase()}${word.substring(1)}').join(' '); 
 }
-
 Text(
   formatCategoryName(category["category"]),
   style: TextStyle(
