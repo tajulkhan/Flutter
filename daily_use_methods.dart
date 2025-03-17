abbb7519dbd01bb90761416288eeec08fb2aa3b5
Tajul Khan BS <78636360+tajulkhan@users.noreply.github.com>
2025-03-17 23:37:10 +0530

    Update daily_use_methods.dart

--- daily_use_methods.dart
+++ daily_use_methods.dart
@@ -244,3 +244,17 @@ void main() async {
   String data = await fetchUserData();
   print(data);
 }
+//Streams (Handling Continuous Data)
+Stream<int> countStream() async* {
+  for (int i = 1; i <= 5; i++) {
+    await Future.delayed(Duration(seconds: 1));
+    yield i;
+  }
+}
+
+void main() async {
+  await for (int value in countStream()) {
+    print(value);
+  }
+}
+
