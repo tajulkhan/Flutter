7f66e9bf8176cacaf7da1e7524bf9fe28bf54bc6
Tajul Khan BS <78636360+tajulkhan@users.noreply.github.com>
2025-03-20 23:05:15 +0530

    Update Transform List with .map()

--- advanced_data_structures.dart
+++ advanced_data_structures.dart
@@ -1,6 +1,7 @@
 // Advanced Data Structures
 
 // 1.Lists & Advanced List Operations
+// Basic List Operations
 void main() {
   List<int> numbers = [10, 20, 30, 40, 50];
 
@@ -13,4 +14,10 @@ void main() {
   numbers.sort(); // Sort the list
 
   print(numbers); // Output: [10, 30, 40, 50, 60]
+}
+// Transform List with .map()
+void main() {
+  List<int> numbers = [1, 2, 3, 4, 5];
+  var squared = numbers.map((n) => n * n).toList();
+  print(squared); // [1, 4, 9, 16, 25]
 }
