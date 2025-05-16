3b3a0c176778f83c5a86b8bfae9e12245ac793ab
Tajul Khan BS <78636360+tajulkhan@users.noreply.github.com>
2025-05-16 10:49:36 +0530

    Update Anonymous functions in Dart

--- anonymous_functions.dart
+++ anonymous_functions.dart
@@ -1,1 +1,11 @@
 // Anonymous Functions
+// Dartprogram to illustrate 
+// Anonymous functions in Dart
+void main()
+{
+    var list = ["Shubham","Nick","Adil","Puthal"];
+    print("GeeksforGeeks - Anonymous function in Dart");
+    list.forEach((item) {
+        print('${list.indexOf(item)} : $item');
+    });
+}
