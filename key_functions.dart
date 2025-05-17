97e82d2bd467725a16c64ea313ca59983d768cef
Tajul Khan BS <78636360+tajulkhan@users.noreply.github.com>
2025-05-18 00:17:09 +0530

    Update Functions with Optional Parameter

--- key_functions.dart
+++ key_functions.dart
@@ -57,3 +57,41 @@ void main()
     // Calling the function
     GFG();
 }
+
+// Functions with Optional Parameter
+void gfg1(int g1, [ var g2 ])
+{
+    // Creating function 1
+    print("g1 is $g1");
+    print("g2 is $g2");
+}
+
+void gfg2(int g1, { var g2, var g3 })
+{
+    // Creating function 1
+    print("g1 is $g1");
+    print("g2 is $g2");
+    print("g3 is $g3");
+}
+
+void gfg3(int g1, { int g2 : 12 })
+{
+    // Creating function 1
+    print("g1 is $g1");
+    print("g2 is $g2");
+}
+
+void main()
+{
+    // Calling the function with optional parameter
+    print("Calling the function with optional parameter:");
+    gfg1(01);
+
+    // Calling the function with Optional Named parameter
+    print("Calling the function with Optional Named parameter:");
+    gfg2(01, g3 : 12);
+
+    // Calling function with default valued parameter
+    print("Calling function with default valued parameter");
+    gfg3(01);
+}
